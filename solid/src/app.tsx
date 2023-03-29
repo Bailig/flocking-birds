013778f0033d19afefecf13404a0fd6f0c5333a5
Bailig <barrybailig@hotmail.com>
2023-03-29 12:39:15 +0300

    created solid js version

--- solid/src/app.tsx
+++ solid/src/app.tsx
@@ -1,3 +1,85 @@
+import { Bird } from "./components";
+import { createSignal, createEffect, For } from "solid-js";
+import * as birdModule from "./modules/bird-mutable";
+
+const RANGE = {
+  x: 640,
+  y: 480,
+};
+
+const COLORS = [
+  "#fca5a5",
+  "#fdba74",
+  "#fcd34d",
+  "#fde047",
+  "#bef264",
+  "#86efac",
+  "#6ee7b7",
+  "#5eead4",
+  "#67e8f9",
+  "#7dd3fc",
+  "#93c5fd",
+  "#a5b4fc",
+  "#c4b5fd",
+  "#d8b4fe",
+  "#f0abfc",
+  "#f9a8d4",
+  "#fda4af",
+];
+
+const TRACK_PER_FRAME = 60;
+const TRACK_TOTAL_COUNT = 60;
+
+const getColor = (frameCount: number) => {
+  const colorIndex = Math.floor(frameCount / TRACK_PER_FRAME) % COLORS.length;
+  return COLORS[colorIndex]!;
+};
+
 export const App = () => {
-  return <div></div>;
+  const [birds, setBirds] = createSignal(
+    birdModule.createRandomBirds(200, RANGE)
+  );
+
+  let frameCount = 0;
+  const timestamps: number[] = [];
+
+  createEffect(() => {
+    birds();
+    if (timestamps.length - 1 > TRACK_TOTAL_COUNT) {
+      console.log(timestamps);
+      return;
+    }
+    setTimeout(() => {
+      setBirds((birds) => {
+        frameCount++;
+        if (frameCount % TRACK_PER_FRAME === 0) {
+          timestamps.push(performance.now());
+        }
+        return birdModule.update(birds, getColor(frameCount), RANGE);
+      });
+    }, 0);
+  });
+
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "grid",
+        "place-content": "center",
+      }}
+    >
+      <div style={{ padding: "32px", border: "1px solid #fff" }}>
+        <div
+          style={{
+            position: "relative",
+            width: `${RANGE.x}px`,
+            height: `${RANGE.y}px`,
+          }}
+        >
+          <For each={birds()}>{(bird) => <Bird {...bird} />}</For>
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ solid/src/components/bird.tsx
@@ -0,0 +1,38 @@
+export const calcRotation = ({ x, y }: { x: number; y: number }) => {
+  const rad = Math.atan2(y, x);
+  const deg = (rad * 180) / Math.PI;
+  return deg + 90;
+};
+
+export type BirdProps = {
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+  next: {
+    x: number;
+    y: number;
+  };
+};
+
+export const Bird = (props: BirdProps) => {
+  const { x, y, size, next, color } = props;
+  const rotation = calcRotation(next);
+
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 100 100"
+      style={{
+        position: "absolute",
+        left: `${x - size / 2}`,
+        top: `${y - size / 2}`,
+        transform: `rotate(${rotation}deg)`,
+        transition: "background-color 0.5s",
+      }}
+    >
+      <polygon points="50,0 0,100 100,100" fill={color} />
+    </svg>
+  );
+};
